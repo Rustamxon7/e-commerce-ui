7bf302e2a09728ef1e4334cb4eb7a79aa8e63fa3
rustamtolipov <rustamxontolipov788@gmail.com>
2023-04-16 20:27:08 +0500

    🎨 TopNavBar style

--- src/utilities/Nav/TopNavBar.jsx
+++ src/utilities/Nav/TopNavBar.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
-
+import styles from './TopNavBar.module.css';
+import { IoPersonOutline, IoCartOutline } from 'react-icons/io5';
 const TopNavBar = () => {
-  return <nav className='top-bar-nav'>Top Nav bar</nav>;
+  return (
+    <nav className={styles.container}>
+      <div className={styles.right}>
+        <div className={styles.logo}>Logo</div>
+        <div className={styles.search}>
+          <input type='text' placeholder='Search any thing' />
+          <button>Search</button>
+        </div>
+      </div>
+
+      <div className={styles.links}>
+        <a href='#'>
+          <IoPersonOutline />
+          <span>Sign in</span>
+        </a>
+        <a href='#'>
+          <IoCartOutline />
+          <span>Cart</span>
+        </a>
+      </div>
+    </nav>
+  );
 };
 
 export default TopNavBar;
