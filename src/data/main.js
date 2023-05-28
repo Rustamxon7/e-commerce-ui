915cf71c07eff3e3a573daa6a922d307c7913801
rustamtolipov <rustamxontolipov788@gmail.com>
2023-05-28 06:44:12 +0500

    Create temporary data file

---
+++ src/data/main.js
@@ -0,0 +1,29 @@
+import cam from './../assets/main/cam.png';
+import phone from './../assets/main/iphone-14.png';
+import laptop from './../assets/main/rog.png';
+import earphones from './../assets/popularProducts/r2.png';
+
+export const new_items = [
+  {
+    id: 0,
+    name: 'Canon 780',
+    count: 6,
+    img: cam,
+    price: 100,
+  },
+  {
+    id: 1,
+    name: 'Asus ROG',
+    count: 2,
+    img: laptop,
+    price: 1200,
+  },
+  {
+    id: 2,
+    name: 'iPhone 14',
+    count: 5,
+    img: phone,
+    price: 300,
+  },
+];
+
