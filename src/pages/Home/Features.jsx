90d7e06a91e126bed0d27a8cb1bd44ba8cb3be83
rustamtolipov <rustamxontolipov788@gmail.com>
2023-06-26 18:54:23 +0500

    Fully complete features section

--- src/pages/Home/Features.jsx
+++ src/pages/Home/Features.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+import styles from './Features.module.css';
+import { AiTwotoneStar } from 'react-icons/ai';
+import { SlBasket } from 'react-icons/sl';
+import { HiCube } from 'react-icons/hi';
+import { FaCrown } from 'react-icons/fa';
+import { BsShieldLockFill } from 'react-icons/bs';
+
+import laptop from '../../assets/popularProducts/laptop.png';
+import cam from '../../assets/popularProducts/laptop.png';
+import phone from '../../assets/popularProducts/phone.png';
 
 const Features = () => {
   return (
-    <div className='features'>Features</div>
-  )
-}
+    <div className={styles.features}>
+      <div className={styles.container}>
+        <div className={styles.card}>
+          <HiCube className={styles.deliver_icon} />
+          <div className={styles.card_info}>
+            <h1 className={styles.warranty}>Free delivery</h1>
+            <p className={styles.describtion}>on order above $50.00</p>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <FaCrown className={styles.deliver_icon} />
 
-export default Features
+          <div className={styles.card_info}>
+            <h1 className={styles.warranty}>Best quality</h1>
+            <p className={styles.describtion}>best quality in low price</p>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <BsShieldLockFill className={styles.deliver_icon} />
+          <div className={styles.card_info}>
+            <h1 className={styles.warranty}>1 year warranty</h1>
+            <p className={styles.describtion}>Avaliable warranty</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Features;
