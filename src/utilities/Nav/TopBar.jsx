9df59232adda0768b8edc27f6d50fd36ac3ef7a5
rustamtolipov <rustamxontolipov788@gmail.com>
2023-06-26 18:51:02 +0500

    Style topbar with queries

--- src/utilities/Nav/TopBar.jsx
+++ src/utilities/Nav/TopBar.jsx
@@ -5,24 +5,26 @@ import styles from './TopBar.module.css';
 
 const TopBar = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.left}>Need help? Call us at 1-800-123-4567</div>
+    <div className={styles.top_bar}>
+      <div className={styles.container}>
+        <div className={styles.left}>Need help? Call us at 1-800-123-4567</div>
 
-      <div className={styles.right}>
-        <ul className={styles.list}>
-          <li className={styles.list__item}>
-            <a href='#' className={styles.list__item__link}>
-              <CiLocationOn className={styles.link__icon} />
-              <span>Our stores</span>
-            </a>
-          </li>
-          <li className={styles.list__item}>
-            <a href='#' className={styles.list__item__link}>
-              <CiDeliveryTruck className={styles.link__icon} />
-              <span>Track your order</span>
-            </a>
-          </li>
-        </ul>
+        <div className={styles.right}>
+          <ul className={styles.list}>
+            <li className={styles.list__item}>
+              <a href='#' className={styles.list__item__link}>
+                <CiLocationOn className={styles.link__icon} />
+                <span>Our stores</span>
+              </a>
+            </li>
+            <li className={styles.list__item}>
+              <a href='#' className={styles.list__item__link}>
+                <CiDeliveryTruck className={styles.link__icon} />
+                <span>Track your order</span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
