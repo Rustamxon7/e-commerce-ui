12dfe80b00ee06b7fbd4953038cc9bf133f8a7ce
rustamtolipov <rustamxontolipov788@gmail.com>
2023-06-26 18:54:04 +0500

    Companies logos with queries

--- src/pages/Home/CompanyLogoSlider.jsx
+++ src/pages/Home/CompanyLogoSlider.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React from 'react';
+import styles from './CompanyLogoSlider.module.css';
+import nike from '../../assets/companies/nike.png';
+import aa from '../../assets/companies/aa.png';
+import bb from '../../assets/companies/bb.png';
+import ab from '../../assets/companies/aa.png';
 const CompanyLogoSlider = () => {
   return (
-    <div className='company-logos'>CompanyLogoSlider</div>
-  )
-}
+    <div className={styles.company_logos}>
+      <div className={styles.container}>
+        <div className={styles.logos}>
+          <>
+            <img src={nike} alt='' />
+            <img src={aa} alt='' />
+            <img src={bb} alt='' />
+            <img src={ab} alt='' />
+            <img src={nike} alt='' />
+          </>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CompanyLogoSlider
+export default CompanyLogoSlider;
